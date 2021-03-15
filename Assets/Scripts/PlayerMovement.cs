8216a5e36f392659a81b34e3e95659011f6155c2
oisin <c20387516@mytudublin.ie>
2021-03-15 00:16:08 +0000

    almost got movement working
    we close

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed, maxSpeed;
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector3.right * moveDir;
+
+        myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
+        moveDir = context.ReadValue<float>();
     }
 
     public void Jump(InputAction.CallbackContext context)
